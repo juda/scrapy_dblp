6e81bb4428347ef4161300ca068a4fe1ca147f07
juda <judaplus@sjtu.edu.cn>
2014-11-27 21:17:35 +0800

    modified

--- tutorial/tutorial/spiders/dblp.py
+++ tutorial/tutorial/spiders/dblp.py
@@ -14,10 +14,13 @@ class dblp(CrawlSpider):
 	start_urls = [
 		'http://dblp.uni-trier.de/pers?pos=1'
 	]
-	rules = [Rule(LinkExtractor(allow=['/pers\?pos=[0-9]+','/pers/hd/[a-z]/.+']), 'parse')]
+	rules = [
+		Rule(LinkExtractor(allow=['/pers\?pos=[0-9]+']) ),
+		Rule(LinkExtractor(allow=['/pers/hd/[a-z]/.+']), 'parse_dblp')
+	]
 
 		
-	def parse(self):
+	def parse_dblp(self):
 
 		sel = Selector(response)
 
@@ -28,10 +31,11 @@ class dblp(CrawlSpider):
 				item = TutorialItem()
 				item['name'] = sel.xpath('/html/head/title/text()').extract()
 				item['article'] = article
+				yield item
 			aritcles = sel.xpath('//*[re:test(@id,"conf/[a-zA-Z]+/[a-zA-Z]*[0-9]+")]/div[3]/span[2]/text()').extract()
 			for aritcle in aritcles:
 				item = TutorialItem()
 				item['name'] = sel.xpath('/html/head/title/text()').extract()
 				item['article'] = article
-		else:
-			return []
+				yield item
+
