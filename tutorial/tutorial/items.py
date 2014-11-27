9483c1aeacb1f250c88829ef009c3910cf6abd70
niconiconi <niconiconi@sjtu.edu.cn>
2014-11-28 00:07:26 +0800

    fix spelling error

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -11,5 +11,5 @@ import scrapy
 class TutorialItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field() #作者名字
-    aritcle = scrapy.Field() #文章名字
+    article = scrapy.Field() #文章名字
     
